6ca16e4a0041fcdf32bbd31cad45c29434e93ab5
Devanshu Sharma <devanshu.sharma@sap.com>
2025-10-29 21:11:31 +0530

    Seller page added

--- fake-product-ui/src/app/seller/seller.ts
+++ fake-product-ui/src/app/seller/seller.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
+import {Router, RouterLink} from '@angular/router';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-seller',
-  imports: [],
+  imports: [
+    NgIf,
+    RouterLink
+  ],
   templateUrl: './seller.html',
   styleUrl: './seller.css',
 })
 export class SellerComponent {
+  isMenuOpen = false;
+  dropdownOpen = false;
+  companyName: string = "Devanshu";
+
+  constructor(private router : Router, private authService: AuthService) {}
+
+  alertMsg() {
+    alert("You are being redirected to the home page.");
+    return true;
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  logout() {
+    // Your logout logic
+    this.authService.logout();
+    this.router.navigate(['/login/seller']);
+    console.log('Logging out...');
+  }
 
 }
