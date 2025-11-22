c35301c2804144d29f218602ed40b818214dce89
Devanshu Sharma <devanshu.sharma@sap.com>
2025-11-22 17:38:35 +0530

    Reusable popup component added

---
+++ fake-product-ui/src/app/shared/popup-dialog/popup-dialog.ts
@@ -0,0 +1,36 @@
+import {ChangeDetectorRef, Component, EventEmitter, Input, Output} from '@angular/core';
+import {CommonModule, NgClass} from '@angular/common';
+
+@Component({
+  selector: 'app-popup-dialog',
+  imports: [
+    NgClass, CommonModule
+  ],
+  templateUrl: './popup-dialog.html',
+  styleUrl: './popup-dialog.css'
+})
+export class PopupDialogComponent {
+  show = false;
+  title = '';
+  message = '';
+  success = false;
+
+  @Output() closed = new EventEmitter<void>();
+
+  constructor(private cdr : ChangeDetectorRef) {
+  }
+
+  open(data: { success: boolean, message: string }) {
+    this.success = data.success;
+    this.title = data.success ? "Transaction Successful!" : "Transaction Failed!";
+    this.message = data.message;
+    this.show = true;
+    console.log("inside open");
+    this.cdr.detectChanges();
+  }
+
+  close() {
+    this.show = false;
+    this.closed.emit();
+  }
+}
