24fcdbc736bbf935f628b6081a00a77f6be5c184
Devanshu Sharma <devanshu.sharma@sap.com>
2025-11-25 01:49:51 +0530

    Solidity config changed to support more stack slots

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -5,7 +5,16 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
   networks: {
     ganache: {
       url: "http://127.0.0.1:7545",
